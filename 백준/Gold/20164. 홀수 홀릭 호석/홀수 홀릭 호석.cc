135731207d768272653d317f58075c7572382c9d
gardenZero <85011588+Garden-Zero@users.noreply.github.com>
2023-11-27 22:42:18 +0900

    [Gold V] Title: 홀수 홀릭 호석, Time: 0 ms, Memory: 2024 KB, Score: 102 point -BaekjoonHub

--- 백준/Gold/20164. 홀수 홀릭 호석/홀수 홀릭 호석.cc
+++ 백준/Gold/20164. 홀수 홀릭 호석/홀수 홀릭 호석.cc
@@ -9,43 +9,36 @@ using namespace std;
 int input;
 int maxInt = 0, minInt = 100000000;
 
-int findLoop(int a);
+void findLoop(int a, int count);
 int horse(string str);
 
 int main(){
     cin >> input;
 
-    findLoop(input);
+    findLoop(input, 0);
 
     cout << minInt << " " << maxInt;
 
     return 0;
 }
 
-int findLoop(int a){
-    int count;
+void findLoop(int a, int count){
+    int nowHorse = horse(to_string(a));
     if(a <= 99 && a >= 10){
-        count = horse(to_string(a)) + findLoop(a/10 + a%10);
+        findLoop(a/10 + a%10 , count + nowHorse);
     }else if(a < 10){
-        count = a%2;
+        count += a%2;
+        if(count < minInt) minInt = count;
+        if(count > maxInt) maxInt = count;
     }else{
         string temp = to_string(a);
         for(int i=1; i<=temp.length()-2; i++){
             for(int j=1; j<=temp.length()-i-1; j++){
-                count = horse(to_string(a));
                 int plus = stoi(temp.substr(0,i)) + stoi(temp.substr(i, j)) + stoi(temp.substr(i+j, temp.length()-i-j));
-                count += findLoop(plus);
-                if(a == input){
-                    if(count > maxInt) maxInt = count;
-                    if(count < minInt) minInt = count;
-                }
+                findLoop(plus, count +  nowHorse);
             }
         }
     }
-
-
-
-    return count;
 }
 
 int horse(string str){
