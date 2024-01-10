6bdaf7ccd7f2575ea78cdb7530075fba7d41cd41
정원영 <85011588+Garden-Zero@users.noreply.github.com>
2024-01-10 20:41:33 +0900

    [Gold I] Title: 멀티탭 스케줄링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.cc
+++ 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.cc
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <queue>
 #include <vector>
 using namespace std;
 
@@ -7,46 +6,43 @@ int hole, action;
 vector<int> plugged;
 vector<int> turn;
 
-bool check(int input, int index){
-    for(int i=0; i<hole; i++){
-        if(plugged[i] == input){
-            return true;
-        }
+bool check(int index){
+    for (int i = 0; i < plugged.size(); i++) {
+        if (plugged[i] == turn[index]) return true;
     }
 
-    for(int i=0; i<hole; i++){
-        if(plugged[i] == 0){
-            plugged[i] = input;
-            return true;
-        }
+    if (plugged.size() < hole){
+        plugged.push_back(turn[index]);
+        return true;
     }
-
-    int unplug=0, lastIdx=-1;
-    for(int i=0; i<hole; i++){
-        int firstIdx = 1000;
-        for(int j = index+1; j<turn.size(); j++){
-            if(plugged[i] == turn[j]){
-                firstIdx = j;
-                break;
+    else {
+        int unplug = 0, lastIdx = -1;
+        for (int i = 0; i < hole; i++) {
+            bool noMore = true;
+            for (int j = index + 1; j < turn.size(); j++) {
+                if (plugged[i] == turn[j]) {
+                    if (lastIdx < j) {
+                        unplug = i;
+                        lastIdx = j;
+                    }
+                    noMore = false;
+                    break;
+                }
+            }
+            if (noMore) {
+                unplug = i; break;
             }
         }
 
-        if(lastIdx < firstIdx){
-            unplug = i;
-            lastIdx = firstIdx;
-        }
+        plugged[unplug] = turn[index];
+        return false;
     }
 
-    plugged[unplug] = input;
-    return false;
+
 }
 
 int main() {
     cin >> hole >> action;
-    for(int i=0; i<hole; i++){
-        plugged.push_back(0);
-    }
-
 
     int input, total = 0;
     for (int i = 0; i < action; i++) {
@@ -55,79 +51,12 @@ int main() {
     }
 
     for (int i = 0; i < action; i++) {
-        if (!check(turn[i], i)){
+        if (!check(i)){
             total++;
         }
     }
 
     cout << total;
 
-
-
     return 0;
 }
-
-
-
-//#include <iostream>
-//#include <queue>
-//#include <vector>
-//using namespace std;
-//
-//int hole, action;
-//vector<int> plugged;
-//int electro[101] = {0, };
-//
-//bool check(int input){
-//    for(int i=0; i<hole; i++){
-//        if(plugged[i] == input){
-//            return true;
-//        }
-//    }
-//
-//    for(int i=0; i<hole; i++){
-//        if(plugged[i] == 0){
-//            plugged[i] = input;
-//            return true;
-//        }
-//    }
-//
-//    int min = 0;
-//    for(int i=0; i<hole; i++){
-//         if(electro[plugged[i]] < electro[plugged[min]]){
-//            min = i;
-//        }
-//    }
-//    plugged[min] = input;
-//    return false;
-//}
-//
-//int main() {
-//    cin >> hole >> action;
-//    for(int i=0; i<hole; i++){
-//        plugged.push_back(0);
-//    }
-//    queue<int> q;
-//
-//    int input, total = 0;
-//    for (int i = 0; i < action; i++) {
-//        cin >> input;
-//        electro[input]++;
-//        q.push(input);
-//    }
-//
-//    for (int i = 0; i < action; i++) {
-//        input = q.front();
-//        q.pop();
-//        electro[input]--;
-//        if (!check(input)){
-//            total++;
-//        }
-//    }
-//
-//    cout << total;
-//
-//
-//
-//    return 0;
-//}
