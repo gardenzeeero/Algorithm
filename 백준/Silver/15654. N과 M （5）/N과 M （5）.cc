07e5daee430d913c2cb1cda99743c1f52765954d
정원영 <85011588+gardenzeeero@users.noreply.github.com>
2024-02-21 00:46:29 +0900

    [Silver III] Title: N과 M (5), Time: 40 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
@@ -0,0 +1,45 @@
+//11분
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m, input;
+vector<int> v;
+vector<bool> isused;
+vector<int> result;
+
+void recursive(int count){
+    if(count == m){
+        for(int i=0; i<m; i++){
+            cout << result[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+    for(int i=0; i<n; i++){
+        if(!isused[i]){
+            isused[i] = true;
+            result[count] = v[i];
+            recursive(count+1);
+            isused[i] = false;
+        }
+    }
+
+}
+
+int main(){
+    cin >> n >> m;
+
+    for(int i=0; i<n; i++){
+        cin >> input;
+        v.push_back(input);
+        isused.push_back(false);
+        result.push_back(input);
+    }
+
+    sort(v.begin(), v.end());
+
+    recursive(0);
+
+    return 0;
+}
