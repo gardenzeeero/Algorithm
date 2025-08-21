1adf85c4457a9fd8450edaea7348c6823c2b7e3b
정원영 <85011588+gardenzeeero@users.noreply.github.com>
2025-08-21 18:39:56 +0900

    [Gold III] Title: 여왕벌, Time: 4496 ms, Memory: 315288 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/10836. 여왕벌/여왕벌.java
+++ 백준/Gold/10836. 여왕벌/여왕벌.java
@@ -4,69 +4,52 @@ import java.util.*;
 public class Main {
 
     static int m, n;
-    static int[][] arr1;
-    static int[][] arr2;
+    static int[][] arr;
     static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         m = input[0]; n = input[1];
-        arr1 = new int[m][m];
-        arr2 = new int[m][m];
+        arr = new int[m][m];
 
         for (int i=0; i<m; i++) {
             for (int j=0; j<m; j++) {
-                arr1[i][j] = 1;
+                arr[i][j] = 1;
             }
         }
 
-        int[][] nextArr = arr2;
-        int[][] prevArr = arr1;
-
-        int leftDiff = 0, upDiff = 0, leftUpDiff = 0, maxDiff = 0;
         for (int i=0; i<n; i++) {
             input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            applyInput(prevArr, nextArr, input);
+            applyInput(arr, input);
+        }
 
+        for (int i=1; i<m; i++) {
             for (int j=1; j<m; j++) {
-                for (int k=1; k<m; k++) {
-                    leftDiff = nextArr[j][k-1] - prevArr[j][k-1];
-                    upDiff = nextArr[j-1][k] - prevArr[j-1][k];
-                    leftUpDiff = nextArr[j-1][k-1] - prevArr[j-1][k-1];
-                    maxDiff = Math.max(Math.max(leftDiff, upDiff), leftUpDiff);
-
-                    nextArr[j][k] = prevArr[j][k] + maxDiff;
-                }
+                arr[i][j] = arr[i-1][j];
             }
-
-            int[][] temp = prevArr;
-            prevArr = nextArr;
-            nextArr = temp;
         }
 
         for (int i=0; i<m; i++) {
             for (int j=0; j<m; j++) {
-                sb.append(prevArr[i][j]).append(" ");
+                sb.append(arr[i][j]).append(" ");
             }
             sb.append("\n");
         }
-
         System.out.println(sb.toString());
-
     }
 
-    static void applyInput(int[][] prevArr, int[][] nextArr, int[] input) {
+    static void applyInput(int[][] arr, int[] input) {
         int idx = 0;
         for (int i=m-1; i>=0; i--) {
-            if (input[idx] == 0) idx++;
-            nextArr[i][0] = prevArr[i][0] + idx;
+            while (input[idx] == 0) idx++;
+            arr[i][0] += idx;
             input[idx]--;
         }
 
         for (int i=1; i<m; i++) {
-            if (input[idx] == 0) idx++;
-            nextArr[0][i] = prevArr[0][i] + idx;
+            while (input[idx] == 0) idx++;
+            arr[0][i] += idx;
             input[idx]--;
         }
     }
