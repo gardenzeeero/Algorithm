124a48500655cb21b425a8b0802d188c121706c2
정원영 <85011588+gardenzeeero@users.noreply.github.com>
2026-01-21 13:56:47 +0900

    [Silver II] Title: 좌표 압축, Time: 1744 ms, Memory: 195580 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -1,36 +1,54 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
+
+    static int n;
+    static int[] result, sorted;
+    static ArrayList<Integer> unique;
+
     public static void main(String[] args) throws IOException {
-        ArrayList<Integer> input = new ArrayList<>();
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        n = Integer.parseInt(br.readLine());
 
-        int n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int[] arr = new int[n];
-        int[] sorted = new int[n];
-        for(int i=0; i<n; i++){
-            arr[i] = Integer.parseInt(st.nextToken());
-            sorted[i] = arr[i];
-        }
+        int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        sorted = new int[n];
+        System.arraycopy(input, 0, sorted, 0, n);
         Arrays.sort(sorted);
 
-        HashMap<Integer, Integer> hm = new HashMap<>();
-        int prev = 1987654321, count = 0;
-        for(int a : sorted){
-            if(a != prev) hm.put(Integer.valueOf(a), Integer.valueOf(count++));
-            prev = a;
+        unique = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (i == 0 || sorted[i] != sorted[i - 1]) {
+                unique.add(sorted[i]);
+            }
         }
 
-
-        for(int i=0; i<n; i++){
-            bw.write(hm.get(Integer.valueOf(arr[i])) + " ");
+        for (int i = 0; i < n; i++) {
+            int count = find(input[i]);
+            bw.write(count + " ");
         }
+        
         bw.flush();
 
+    }
+
+    static int find(int target) {
+        int left = 0;
+        int right = unique.size() - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (unique.get(mid) == target) return mid;
+            else if (unique.get(mid) < target) left = mid + 1;
+            else right = mid - 1;
+        }
+
+        return left;
     }
 }
