0be7e3f3a70f7f476bf7b8a1a7e82d7cec6783f4
정원영 <85011588+gardenzeeero@users.noreply.github.com>
2026-02-24 11:06:56 +0900

    [Gold II] Title: 연료 채우기, Time: 216 ms, Memory: 19976 KB -BaekjoonHub

--- 백준/Gold/1826. 연료 채우기/연료 채우기.java
+++ 백준/Gold/1826. 연료 채우기/연료 채우기.java
@@ -1,76 +1,55 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
-import static java.lang.System.in;
-import static java.util.Arrays.stream;
-
-
-/**
- * dp인데 이전 값이 필요 X -> 한사이클 돌때마다 연료 -1
- * 있어야하는 정보 = 현재위치 -> 어차피 한사이클이니까 필요 X
- * 연료 + 교환횟수 -> 교환횟수가 같으면 연료만 있으면 되는거 아닌가? -> dp니까 굳이 같은 연료에 교환횟수가 있을 이유가
- * 현재위치에서 -> 최대연료 + 최소 환승 -> 최소 환승이지만 연료가 부족하면? -> dp로 해결안되는것 같긴한데
- * 주유소가 10000개면 배열에 총 넣냐 안넣냐 -> 2의 10000승개가 들어가나?
- * 위치 + 연료 -> 최소 환승 이렇게 해야하는데 위치가 100만 * 100 * 10000 아무튼 존나 많아
- * equals를 선언하면 되긴한데 arrayList 에다가 hashmap으로 하면 될듯
- */
 public class Main {
 
-    //key가 교환횟수, value가 연
-    static int[] oil;
-    static TreeMap<Integer, Integer> stations = new TreeMap<>();
-    static int n, l, p;
-
-
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(in));
-        int[] input = stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        n = input[0];
-        oil = new int[n + 1];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
+        PriorityQueue<GasStation> q = new PriorityQueue<>((a, b) -> Integer.compare(a.pos, b.pos));
+        PriorityQueue<GasStation> passed = new PriorityQueue<>((a, b) -> Integer.compare(b.fuel, a.fuel));
         for (int i = 0; i < n; i++) {
-            input = stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            stations.put(input[0], input[1]);
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int pos = Integer.parseInt(st.nextToken());
+            int fuel = Integer.parseInt(st.nextToken());
+            q.add(new GasStation(pos, fuel));
         }
 
-        input = stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        l = input[0];
-        p = input[1];
-
-        for (int i = 0; i <= n; i++) {
-            oil[i] = -1;
-        }
-
-        oil[0] = p;
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int cur = 0;
-        for (Map.Entry<Integer, Integer> entry : stations.entrySet()) {
-            Integer stationPos = entry.getKey();
-            for (int i = 0; i <= n; i++) {
-                if (oil[i] < stationPos - cur) oil[i] = -1;
-                else oil[i] -= stationPos - cur;
+        int dest = Integer.parseInt(st.nextToken());
+        int fuel = Integer.parseInt(st.nextToken());
+
+        int count = 0;
+        while (true) {
+            cur += fuel;
+            if (cur >= dest) break;
+            while (!q.isEmpty() && q.peek().pos <= cur) {
+                passed.add(q.poll());
             }
 
-            for (int i = n; i >= 0; i--) {
-                if (oil[i] == -1) continue;
-                if (oil[i + 1] == 0) oil[i + 1] = oil[i] + entry.getValue();
-                else oil[i + 1] = Math.max(oil[i] + entry.getValue(), oil[i + 1]);
+            if (passed.isEmpty()) {
+                count = -1;
+                break;
             }
 
-            cur = stationPos;
+            fuel = passed.poll().fuel;
+            count++;
         }
 
-        for (int i = 0; i <= n; i++) {
-            if (oil[i] < l - cur || oil[i] == -1) continue;
-            else {
-                System.out.println(i);
-                return;
-            }
+        System.out.println(count);
+    }
+
+    static class GasStation {
+        int pos, fuel;
+
+        public GasStation(int pos, int fuel) {
+            this.pos = pos;
+            this.fuel = fuel;
         }
-
-        System.out.println(-1);
-
     }
 }
